616a14700090220af661cb8fe8d3c4b757c1efdf
OpenCV13 <dong@hood.edu>
2022-04-11 20:44:41 +0000

    song change

--- test-run-2/song.sql
+++ test-run-2/song.sql
@@ -1,4 +1,4 @@
 use database-hood-db;
 drop table if exists songs;
 create table songs(id int NOT NULL AUTO_INCREMENT primary key,  title varchar(50), artist varchar(50), genre varchar(20));
-insert into songs (title, artist, genre) values ("title 1", "title 1 artist", "title genre"), ("title 2", "title 2 artist", "title 2 genre"), ("title 3", "title 3 artist", "title 3 genre");
+insert into songs (title, artist, genre) values ("Five Hundred Miles", "The Kingston Trio", "Folk"), ("Yesterday Once More ", "The Carpenters", "pop"), ("A Whole New World ", "Alan Menken", "R&B, Soul");
