eb5a5fa72a7cbbe5ebff97a752e48ed5aa9fb4ae
matthewcurran <matthewcurran@users.noreply.github.com>
2024-04-09 17:27:09 -0400

    Update song.sql

--- test-run-3/song.sql
+++ test-run-3/song.sql
@@ -1,4 +1,4 @@
-use database-hood-db;
+use database_hood_mjc15;
 drop table if exists songs;
 create table songs(id int NOT NULL AUTO_INCREMENT primary key,  title varchar(50), artist varchar(50), genre varchar(20));
 insert into songs (title, artist, genre) values ("Five Hundred Miles", "The Kingston Trio", "Folk"), ("Yesterday Once More ", "The Carpenters", "pop"), ("A Whole New World ", "Alan Menken", "R&B, Soul");
